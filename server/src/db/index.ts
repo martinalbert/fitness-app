db744ff9f5d4c47a8cdcc6385dbbb60a053e5594
Martin Albert <martin.albert.187@gmail.com>
2020-07-16 17:28:44 +0200

    exported ActivityRepo class

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -16,6 +16,23 @@ import sequelize from './sequelize'
 export const UserRepo = loader.loadRepo('user')
 
 /**
+ * Repository Class\
+ * Entity: Activity\
+ * Class that handles communication with database
+ *
+ * @class ActivityRepo
+ * @extends IActivityRepo
+ * @function getByID - Function that finds one activity represented by its ID
+ * @function getAll - Function that finds all activities corresponding to its user
+ * @function getAllByType - Function that finds all activities corresponding to its user and its type
+ * @function getLastX - Function that finds last X activities corresponding to specified number
+ * @function create - Function that creates new activity corresponding to activity passed in
+ * @function update - Function that updates old activity corresponding to activity properties passed in
+ * @function delete - Function that deletes activity represented by its ID
+ */
+export const ActivityRepo = loader.loadRepo('activity')
+
+/**
  * Instance of Sequelize\
  * Represent connection to database.
  *
