9930fa5c8766dd6d5ea5b84fba675e2ff792863d
Martin Albert <martin.albert.187@gmail.com>
2020-07-16 17:27:38 +0200

    added seed function and sample data for activity

--- server/src/db/repos/postgres/seed.ts
+++ server/src/db/repos/postgres/seed.ts
@@ -1,13 +1,19 @@
 import bcrypt from 'bcryptjs'
 import data from '../../sampleData'
-import { getLastUserID } from './actions'
+import { getLastUserID, getLastActivityID } from './actions'
 import UserRepo from './user_postgres_repo'
+import ActivityRepo from './activity_postgres_repo'
 
 /**
  * Array of sample Users
  * @constant {User[]} users
  */
 const users: IUser[] = data.users
+/**
+ * Array of sample Activities
+ * @constant {Activity[]} activities
+ */
+const activities: IActivity[] = data.activities
 
 const registerUser = async (user: IUser) => {
     const userRepo = new UserRepo()
@@ -39,6 +45,20 @@ const seedUsers = async (): Promise<boolean> => {
     }
 }
 
+const seedActivities = async (): Promise<boolean> => {
+    const activityRepo = new ActivityRepo()
+    const activityCount = await getLastActivityID()
+
+    if (activityCount < activities.length) {
+        for (const activity of activities) {
+            await activityRepo.create(activity)
+        }
+        return true
+    } else {
+        return false
+    }
+}
+
 /**
  * Function that seed base two Users Applifting and Batman\
  * and base few images that can be monitored.
@@ -49,6 +69,8 @@ const seed = async (): Promise<void> => {
     try {
         await seedUsers()
         console.log('users have been seeded')
+        await seedActivities()
+        console.log('activities have been seeded')
     } catch (error) {
         throw Error(error)
     }
--- server/src/db/sampleData.ts
+++ server/src/db/sampleData.ts
@@ -1,8 +1,29 @@
 import User from '../entities/User'
+import Activity from '../entities/Activity'
 
 const user1 = new User(1, 'Martin', 'info@martin.com', 'password12345')
 const user2 = new User(2, 'Batman', 'batman@example.com', '12345password')
 
+const activity1 = new Activity(1, 'crossfit', 'testing', 120, new Date(), 'ZA', user1)
+const activity2 = new Activity(2, 'jogging', 'testing', 120, new Date(), 'ZA', user1)
+const activity3 = new Activity(3, 'walking', 'testing', 120, new Date(), 'ZA', user1)
+const activity4 = new Activity(4, 'yoga', 'testing', 120, new Date(), 'ZA', user1)
+
+const activity5 = new Activity(5, 'workout', 'testing', 120, new Date(), 'ZA', user2)
+const activity6 = new Activity(6, 'jogging', 'testing', 120, new Date(), 'ZA', user2)
+const activity7 = new Activity(7, 'walking', 'testing', 120, new Date(), 'ZA', user2)
+const activity8 = new Activity(8, 'crossfit', 'testing', 120, new Date(), 'ZA', user2)
+
 export default {
     users: [user1, user2],
+    activities: [
+        activity1,
+        activity2,
+        activity3,
+        activity4,
+        activity5,
+        activity6,
+        activity7,
+        activity8,
+    ],
 }
